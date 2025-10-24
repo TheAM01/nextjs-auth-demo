9390b2b247d681071c72600253e1d1e8eb18ca8d
TheAM01 <onevisionofficial55@gmail.com>
2025-10-24 20:29:24 +0500

    Changed color on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <form
         action={signInToApp}
       >
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">Sign in with Google</button>
+        <button className="bg-blue-800 text-white px-4 py-2 rounded">Sign in with Google</button>
       </form>
       <form
         action={signOutOfApp}
