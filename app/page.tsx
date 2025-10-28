d094faadf5fa7f071d220c8f0f191fe42dbde666
TheAM01 <onevisionofficial55@gmail.com>
2025-10-28 20:41:25 +0500

    Made changes to login page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import { signInToApp, signOutOfApp } from "./actions/auth-actions";
 
+const example: string = "123";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen space-y-4">
-      <h1 className="text-2xl font-bold">Next.js & Auth.js</h1>
+      <h1 className="text-2xl font-bold">Next.js & Auth.js Demo</h1>
       <div className="text-sm text-neutral-400">This app is a demonstration of authentication in Next.js using Auth.js and Git & GitHub!</div>
+      <div className="text-sm text-neutral-400">This is also my first time using Vercel for deployment!</div>
       <form
         action={signInToApp}
       >
